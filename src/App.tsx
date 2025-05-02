f9cf756c0cefd7a37805a86a9c5f0e0015a1c55f
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-02 10:55:04 -0300

    feat: the redirected routes from the application to their distinct pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Layout } from './components/Layout';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { ThemeProvider } from "./context/theme-provider";
+import { WeatherDashboard } from "./pages/weather-dashboard";
+import { CityPage } from "./pages/city-pages";
 function App() {
-
   return (
     <BrowserRouter>
-      <Layout>
-        
-      </Layout>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
