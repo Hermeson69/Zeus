08cda9d6824bb8f2df9671d2211164a89259c5a6
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-02 14:02:49 -0300

    feat: start weather page

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,8 +1,26 @@
-export const WeatherDashboard = () => {
+import { Button } from "@/components/ui/button"
+import { RefreshCw } from "lucide-react"
 
+export const WeatherDashboard = () => {
     return(
-        <div>
-            To aqui
+        <div space-y-4>
+            {/*Favorite cities */}
+           <div className="flex items-center justify-between">
+            <h1 className="text-xl font-bold tracking-tight">
+                My Location
+            </h1>
+                <Button
+                variant={`outline`}
+                size={"icon"}
+                // onClick={handleRefresh}
+                // disabled={}
+                >
+                    <RefreshCw className="h-4 w-4" />
+                </Button>
+          
+           </div>
+
+           {/*Current and hourly weather */}
         </div>
     )
 
