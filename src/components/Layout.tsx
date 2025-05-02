ffb89bbb40bf06c953a2492cd1fee9735781581f
Hermeson Alves <hermesonalves256@gmail.com>
2025-05-02 10:53:37 -0300

    fear: ajust in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,11 @@ export const Layout = ({ children }: PropsWithChildren) => {
       <main className="min-h-dvh container mx-auto px-4 py-8">
       {children}
       </main>
-      footer
+      <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
+        <div className="container mx-auto px-4 text-center text-gray-400">
+          <p> &copy; Zeus</p>
+        </div>
+      </footer>
     </div>
   );
 };
