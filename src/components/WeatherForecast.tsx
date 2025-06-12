d1729caaba440790f28f858529437f50cedd0f96
Hermeson Alves <hermesonalves256@gmail.com>
2025-06-12 13:49:35 -0300

    feat: start a Weather foreecast

--- src/components/WeatherForecast.tsx
+++ src/components/WeatherForecast.tsx
@@ -1,14 +1,32 @@
 import type { ForecastData } from "@/api/types";
 import { Card } from "./ui/card";
+import { format } from "date-fns";
 
 interface WeatherForecastProps {
     data: ForecastData;
 }
 
 export function WeatherForecast({ data }: WeatherForecastProps) {
+
+    const dailyForcasts = data.list.reduce((acc, forecast)=>{
+        const date = format(new Date(forecast.dt * 1000), "yyyy, MM, dd");
+
+        if(!acc[date]){
+            acc[date] = {
+                temp_min : forecast.main.temp_min,
+                temp_max : forecast.main.temp_max,
+                humidity : forecast.main.humidity,
+                wind: forecast.wind.speed,
+                weather: forecast.weather[0],
+                date: forecast.dt,
+            }
+        }else{
+            
+        }
+    }, {});
     return (
         <Card>
-            
+
         </Card>
     );
 }
